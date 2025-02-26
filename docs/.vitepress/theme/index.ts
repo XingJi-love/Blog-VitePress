6cdd7cea9678660c6d3d734da3fd74a086c5e90e
XingJi-love <3968523679@qq.com>
2025-02-26 23:51:22 +0800

    更新彩虹背景动画使用

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -50,6 +50,9 @@ import Carousel from "./components/Carousel.vue"; // 轮播图组件
 
 import LoveTimer from "./components/LoveTimer.vue"; // 爱情倒计时组件
 
+// 彩虹背景动画样式
+let homePageStyle: HTMLStyleElement | undefined;
+
 const playlist = [
   {
     name: "白鸽乌鸦相爱的戏码-潘成（皮卡潘）",
@@ -230,6 +233,33 @@ export default {
         busuanzi.fetch();
         NProgress.done(); // 停止进度条
       };
+    }
+
+    // 彩虹背景动画样式
+    if (typeof window !== "undefined") {
+      watch(
+        () => router.route.data.relativePath,
+        () => updateHomePageStyle(location.pathname === "/"),
+        { immediate: true }
+      );
+    }
+    // 彩虹背景动画样式
+    function updateHomePageStyle(value: boolean) {
+      if (value) {
+        if (homePageStyle) return;
+
+        homePageStyle = document.createElement("style");
+        homePageStyle.innerHTML = `
+    :root {
+      animation: rainbow 12s linear infinite;
+    }`;
+        document.body.appendChild(homePageStyle);
+      } else {
+        if (!homePageStyle) return;
+
+        homePageStyle.remove();
+        homePageStyle = undefined;
+      }
     }
   },
 };
